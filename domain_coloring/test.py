aebf4c54f6c61fbef1f9a186b38c92a71b64f484
sfmiller940 <sfm15@columbia.edu>
2015-10-02 16:21:56 -0700

    Update test.py

--- domain_coloring/test.py
+++ domain_coloring/test.py
@@ -4,20 +4,24 @@ import math as math
 import numpy as np
 import cmath as cmath
 
-size = 800
-width = 13.0
-frames = 20
+size = 400
+width = 11.0
+frames = 100
 
 for frame in range(frames):
-	img = Image.new( 'RGB', (size,size), "black") # create a new black image
+	img = Image.new( 'RGB', ( 3 * size / 2 ,size), "black") # create a new black image
 	pixels = img.load() # create the pixel map
+	gamma = 0.5 - ( 0.5 * math.cos( frame * 2 * math.pi / frames ) )
 
 	for i in range(img.size[0]):    # for every pixel:
 	    for j in range(img.size[1]):
-	        x = ( width * i / size ) - ( width / 2 )
-	        y = ( width * j / size ) - ( width / 2 )
-	        z = x + ( 1.0j * y )
-	        z = cmath.tan(z) + (frame * cmath.exp(z) / frames )
+	        x = 1.5 * ( ( width * i / img.size[0] ) - ( width / 2 ) )
+	        y = ( width * j / img.size[1] ) - ( width / 2 )
+	        z = y + ( 1.0j * x )
+	        if frame < frames / 2:
+	        	z = z + ( gamma * ( cmath.tan( z ) + cmath.exp( z * gamma ) ) ) 
+	        else:
+	        	z = z + ( gamma * ( cmath.tan( gamma * z ) + cmath.exp( z ) ) ) 
 	        phase = cmath.phase(z)
 	        if phase < 0 : phase = ( 2 * math.pi ) + phase
 	        H = phase / (2 * math.pi)
@@ -27,4 +31,5 @@ for frame in range(frames):
 	        for k in range(3): color[k] = int( 255 * color[k] )
 	        pixels[i,j] = tuple(color) # set the colour accordingly
 
-	img.show()
+	#img.show()
+	img.save("expintanout{:04}.png".format(frame))
